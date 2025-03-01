e2199c7d99d4feb36adbef952c2e420ef9328a47
fai7h01 <l.varsimashvil10@gmail.com>
2025-03-01 23:10:43 +0400

    runner

--- src/main/java/com/ced/costefficientdeplyment/CostEfficientDeploymentApplication.java
+++ src/main/java/com/ced/costefficientdeplyment/CostEfficientDeploymentApplication.java
@@ -1,6 +1,6 @@
 package com.ced.costefficientdeplyment;
 
-import com.ced.costefficientdeplyment.service.DataLoadingService;
+import com.ced.costefficientdeplyment.util.DataProcessUtil;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,20 +9,20 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CostEfficientDeploymentApplication {
 
-    public CostEfficientDeploymentApplication(DataLoadingService dataLoadingService) {
-        this.dataLoadingService = dataLoadingService;
+    public CostEfficientDeploymentApplication(DataProcessUtil dataProcessUtil) {
+        this.dataProcessUtil = dataProcessUtil;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(CostEfficientDeploymentApplication.class, args);
     }
 
-    private final DataLoadingService dataLoadingService;
+    private final DataProcessUtil dataProcessUtil;
 
     @Bean
     public CommandLineRunner commandLineRunner() {
         return args -> {
-            dataLoadingService.processPipelinesDataset();
+            dataProcessUtil.processEmptyPipelineDataset();
         };
     }
 }
