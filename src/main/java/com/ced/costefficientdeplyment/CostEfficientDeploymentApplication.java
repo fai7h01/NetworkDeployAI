c25a16be7c64f40d4b59b347ae01c243a3aca7bf
fai7h01 <l.varsimashvil10@gmail.com>
2025-03-02 16:38:47 +0400

    build

--- src/main/java/com/ced/costefficientdeplyment/CostEfficientDeploymentApplication.java
+++ src/main/java/com/ced/costefficientdeplyment/CostEfficientDeploymentApplication.java
@@ -3,14 +3,17 @@ package com.ced.costefficientdeplyment;
 import com.ced.costefficientdeplyment.service.PipelineService;
 import com.ced.costefficientdeplyment.util.DataProcessUtil;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Profile("prod")
 @SpringBootApplication
 public class CostEfficientDeploymentApplication {
 
@@ -22,8 +25,13 @@ public class CostEfficientDeploymentApplication {
         SpringApplication.run(CostEfficientDeploymentApplication.class, args);
     }
 
-    private static final String EMPTY_PIPELINES_PATH = "C:\\Users\\user\\Desktop\\cost-efficient-deplyment\\src\\main\\resources\\Canalitzacions_de_xarxes_de_telecomunicacions_de_la_Generalitat_20250228.csv";
-    private static final String NON_EMPTY_PIPELINES_PATH = "C:\\Users\\user\\Desktop\\cost-efficient-deplyment\\src\\main\\resources\\part2.csv";
+    //private static final String EMPTY_PIPELINES_PATH = "C:\\Users\\user\\Desktop\\cost-efficient-deplyment\\src\\main\\resources\\Canalitzacions_de_xarxes_de_telecomunicacions_de_la_Generalitat_20250228.csv";
+    //private static final String NON_EMPTY_PIPELINES_PATH = "C:\\Users\\user\\Desktop\\cost-efficient-deplyment\\src\\main\\resources\\part2.csv";
+
+    @Value("${file.path.empty}")
+    private String path1;
+    @Value("${file.path.non-empty}")
+    private String path2;
 
     private static final String SIMPLE_PATTERN = "\\(\\((.*?)\\)\\)";
 
@@ -32,8 +40,8 @@ public class CostEfficientDeploymentApplication {
     @Bean
     public CommandLineRunner commandLineRunner() {
         return args -> {
-            pipelineService.savePipelines(NON_EMPTY_PIPELINES_PATH, SIMPLE_PATTERN, true);
-            pipelineService.savePipelines(EMPTY_PIPELINES_PATH, SIMPLE_PATTERN, false);
+            pipelineService.savePipelines(path2, SIMPLE_PATTERN, true);
+            pipelineService.savePipelines(path1, SIMPLE_PATTERN, false);
         };
     }
 
